95219180928a5a5878a97da40e12260b1524f3d7
hunaonao <1594062324@qq.com>
2025-11-14 14:44:28 +0800

    优化启动脚本

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-nohup python3 app.py -c config.yaml  >> ./alertmanager-webhook.log 2>&1 &
+#!/bin/bash
+nohup python3 app.py -c config.yaml > /dev/null 2>&1 &
+PID=$!
+echo $PID > alertmanager-webhook.pid
+echo "服务已启动，PID: $PID"
+
